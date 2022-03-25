9e1c6a44d29457b2774b2ec205c66951a6894f3a
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-25 15:22:12 +0600

    added files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>THis is header</h1>
+        </div>
+    );
+};
+
+export default Header;
